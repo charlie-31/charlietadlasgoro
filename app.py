422d68a7f716db9ee71ce605ca42a7ce5023359b
charlie-31 <charlietadlas31@gmail.com>
2026-03-13 10:09:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 from flask import Flask, jsonify, request
-
 app = Flask(__name__)
-
 students = [
     {"id": 1, "name": "John Cruz", "grade": 10, "section": "Zechariah"},
     {"id": 2, "name": "Maria Santos", "grade": 10, "section": "Matthew"}
 ]
-
 @app.route('/')
 def home():
     return "Student Record API"
-
 @app.route('/students', methods=['GET'])
 def get_students():
     return jsonify(students)
-
 @app.route('/students/<int:id>', methods=['GET'])
 def get_student(id):
     for student in students:
         if student["id"] == id:
             return jsonify(student)
     return jsonify({"message": "Student not found"})
-
 @app.route('/students', methods=['POST'])
 def add_student():
     new_student = {
@@ -32,6 +26,6 @@ def add_student():
     }
     students.append(new_student)
     return jsonify(new_student)
-
 if __name__ == "__main__":
     app.run(debug=True)
+
